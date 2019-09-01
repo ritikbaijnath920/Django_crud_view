4844036411e28f6d97550ee2083a5b9e4819f629
ritikbaijnath920 <ritikbaijnath920@gmail.com>
2019-09-01 19:06:23 +0530

    first commit

---
+++ basic_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from basic_app import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    url(r'^$',views.SchoolListView.as_view(),name='list'),
+    url(r'^(?P<pk>\d+)/$',views.SchoolDetailView.as_view(),name='detail'),
+    #url(r'^post/(?P<pk>\d+)/$',
+    #url(r'^post/(?P<pk>\d+)/edit/$',views.SchoolDetailView.as_view(),name='detail')
+    url(r'^create/$',views.SchoolCreateView.as_view(),name='create'),
+    url(r'^update/(?P<pk>\d+)/$',views.SchoolUpdateView.as_view(),name='update'),
+    url(r'^delete/(?P<pk>\d+)/$',views.SchoolDeleteView.as_view(),name='delete'),
+
+]
+
+
